38822e1fd7a4bfd96e4369a23195c0177de31759
deka381 <konrad381@gmail.com>
2017-07-06 13:51:50 +0200

    api weather full

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(function () {
   var inputCiti=$("#citiInput");
   var btn=$(".btn");
   var table=$(".weather");
+  var divMap = $(".maps");
   var tr;
   var cityName;
   var temp;
@@ -14,7 +15,8 @@ $(function () {
     event.preventDefault();
     var textInput=inputCiti.val();
     var apiUrl='http://api.apixu.com/v1/current.json?key=6dcad8a6de714b7396a164032170507&q='+textInput;
-    function ShowWeather(citis) {
+    divMap.find('h3').removeClass("look");
+      function ShowWeather(citis) {
       var iconUrl=citis.current.condition.icon;
       tr=$('<tr>');
       td=$('<td>');
@@ -34,16 +36,43 @@ $(function () {
       tr.prev().remove();
     }
 
+
+
+    function initMap(place) {
+
+    ;
+
+
+      var uluru = {lat: place.location.lat, lng: place.location.lon};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    }
+
+
+
+
+
+
+
+
     function LoadCiti() {
       $.ajax({
         url:apiUrl
       }).done(function (response) {
         ShowWeather(response);
+        initMap(response);
       }).fail(function (err) {
 
       })
     }
     LoadCiti();
+    initMap();
 
   })
 
