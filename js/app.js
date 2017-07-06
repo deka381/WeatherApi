dc5470e6937274fe7a06ca24bcab05afc3559a63
deka381 <konrad381@gmail.com>
2017-07-06 11:55:49 +0200

    api reade/without css

--- js/app.js
+++ js/app.js
@@ -2,39 +2,52 @@
 $(function () {
   var inputCiti=$("#citiInput");
   var btn=$(".btn");
-  var ul=$(".weather");
+  var table=$(".weather");
+  var tr;
+  var cityName;
+  var temp;
+  var icon;
+  var textC;
+  var st;
+    btn.on("click",function (event) {
 
-
-  btn.on("click",function (event) {
-    // span.text('');
     event.preventDefault();
-    console.log(textInput);
     var textInput=inputCiti.val();
     var apiUrl='http://api.apixu.com/v1/current.json?key=6dcad8a6de714b7396a164032170507&q='+textInput;
-    console.log(apiUrl);
-
+    var sup1=$("<sup>");
     function ShowWeather(citis) {
-      var li = $('<li>');
-      var span = $('<span>').text(citis.name);
-      var temp = $('<span>').text(citis.temp_c);
-      li.append(span);
-      li.append(temp);
-      ul.append(li);
+      var iconUrl=citis.current.condition.icon;
+      tr=$('<tr>');
+      td=$('<td>');
+      cityName = $('<td><span>').text(citis.location.name);
+      st= $('<sup>').text("o");
+      textC=$('<span>').text("C");
+      temp = $('<td><span>').text(citis.current.temp_c).append(st).append(textC);
+      cloud=$("<td><span>").text("zachmurzenie: " + citis.current.cloud + " %");
+      icon=$('<img>').attr("src",'http://'+iconUrl.substr(2));
+      td.append(icon);
+      tr.append(cityName);
+      tr.append(cloud);
+      // temp.append(st);
+      tr.append(temp);
+      tr.append(td);
+      table.append(tr);
+
+      tr.prev().remove();
     }
 
     function LoadCiti() {
       $.ajax({
         url:apiUrl
       }).done(function (response) {
-        ShowWeather(response.location);
-        ShowWeather(response.current);
+        ShowWeather(response);
       }).fail(function (err) {
-        console.log("errror");
+
       })
     }
     LoadCiti();
-  })
 
+  })
 
 
 
